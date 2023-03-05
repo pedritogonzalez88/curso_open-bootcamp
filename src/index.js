4d81e14ea32998027e9eb8718a3006876738cf37
Pedro <viento100@gmail.com>
2023-03-05 17:59:04 -0300

    agregando informacion de perfil

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log("Hola Este es mi primer ejercicio con Node en el mejor Bootcamp de Programacion del Mundo");
+
+
---
+++ src/perfil.js
@@ -0,0 +1,18 @@
+//modelo de datos
+const perfil = {
+    nombre: "Pedro Daniel",
+    apellido: "Gonzalez Meza",
+    edad: 34,
+    desarrollador: true,
+    fechaNacimiento: new Date("12/06/1988"),
+    Libro: {
+        titulo: "Harry Potter y la piedra Filosofal",
+        author: "JK Rowling",
+        fecha: new Date("12/08/2015"),
+        url: "https://www.amazon.com/J-K-Rowling-ebook/dp/B0192CTNJ0/ref=zg_bs_7588769011_sccl_49/144-6014975-8660518?psc=1"
+    }
+
+
+}
+
+console.log(perfil);
